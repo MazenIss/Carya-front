12258ac77caf9038bac95b38b9ecbc62bc671c42
7MZV <hamzasdiri@insat.u-carthage.tn>
2022-05-31 05:54:54 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     HomePageComponent,
+    CarouselComponent,
   ],
   imports: [
     BrowserModule,
